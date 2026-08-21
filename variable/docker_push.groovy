53588cb151fe7ce92ef15a93cabc75779cc10ae3
Devashish Pandey <142854951+devashish-pandey@users.noreply.github.com>
2026-08-21 18:24:24 +0530

    Add Docker push functionality with credentials

--- variable/docker_push.groovy
+++ variable/docker_push.groovy
@@ -1,1 +1,11 @@
-
+def call (String test , String imgName) {
+  withCredentials([usernamePassword(
+                    credentialsId: "${test}",
+                    passwordVariable: "dockerHubPass",
+                    usernameVariable: "dockerHubUser"
+                    )]){
+                        sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+                        sh "docker tag ${imgName}:latest ${env.dockerHubUser}/${imgName}:latest"
+                        sh "docker push ${env.dockerHubUser}/${imgName}:latest"
+                    }
+}
